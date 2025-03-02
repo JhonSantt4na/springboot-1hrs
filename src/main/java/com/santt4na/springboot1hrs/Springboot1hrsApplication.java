45492ba8819132ab2f9970a0aebfa3ec36e7c58f
Santt4na <jhonnsantt4na@gmail.com>
2025-03-02 17:35:51 -0300

    Started Project

---
+++ src/main/java/com/santt4na/springboot1hrs/Springboot1hrsApplication.java
@@ -0,0 +1,13 @@
+package com.santt4na.springboot1hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot1hrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot1hrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/santt4na/springboot1hrs/Springboot1hrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santt4na.springboot1hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot1hrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
